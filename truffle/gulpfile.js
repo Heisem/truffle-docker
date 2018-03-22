035bd859a39bb5e208aa843ee33d32d0a22b6bb4
Carlos Parra <carlos.parra@nearshoresystems.com>
2018-03-22 13:35:20 -0300

    .

--- truffle/gulpfile.js
+++ truffle/gulpfile.js
@@ -5,7 +5,7 @@ const shell = require('gulp-shell');
 
 gulp.task('default', shell.task('npm start'));
 
-gulp.task('truffle-shell', shell.task('npm run watch'))
+gulp.task('truffle-shell', shell.task('npm run watch'));
 
 gulp.task('watch', ['default'], () => {
   gulp.watch(['./contracts/*.sol'], ['truffle-shell']);
