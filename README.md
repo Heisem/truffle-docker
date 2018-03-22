c4227d1d193887a19eec5ce83bde3c3aadd351d5
Carlos Parra <carlos.parra@nearshoresystems.com>
2018-03-22 14:19:58 -0300

    .

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 
 #### How to use:
   - run: `docker-compose up -d`
+#### How to quickly test deployment:
+  - run: `docker exec -ti truffle /bin/bash`
+  - run: `npm run console`
--- truffle/gulpfile.js
+++ truffle/gulpfile.js
@@ -5,7 +5,7 @@ const shell = require('gulp-shell');
 
 gulp.task('default', shell.task('npm start'));
 
-gulp.task('truffle-shell', shell.task('npm run watch'));
+gulp.task('truffle-shell', shell.task('npm run start'));
 
 gulp.task('watch', ['default'], () => {
   gulp.watch(['./contracts/*.sol', './migrations/*.js'], ['truffle-shell']);
