10d212aae313a46d2cdfc8583b33b1caef48181d
Carlos Parra <carlos.parra@nearshoresystems.com>
2018-03-28 11:30:01 -0300

    added ignoreErrors and verbose

--- truffle/gulpfile.js
+++ truffle/gulpfile.js
@@ -3,9 +3,15 @@
 const gulp  = require('gulp');
 const shell = require('gulp-shell');
 
-gulp.task('default', shell.task('npm start'));
+gulp.task('default', shell.task('npm start', {
+  ignoreErrors: true,
+  verbose: true,
+}));
 
-gulp.task('truffle-shell', shell.task('npm run compile && npm run migrate'));
+gulp.task('truffle-shell', shell.task('npm run compile && npm run migrate', {
+  ignoreErrors: true,
+  verbose: true,
+}));
 
 gulp.task('watch', ['default'], () => {
   gulp.watch(['./contracts/*.sol', './migrations/*.js'], ['truffle-shell']);
